600c2680126cb784c94159ead439d43ea9e0ff16
Debora Tristao <48920353+debytristao@users.noreply.github.com>
2019-04-30 15:42:50 -0300

    Sorted List Exercise
    Sorted List Exercise

--- SortedListExTask/Form1.cs
+++ SortedListExTask/Form1.cs
@@ -16,5 +16,70 @@ namespace SortedListExTask
         {
             InitializeComponent();
         }
+        private SortedList<DateTime, string> taskList = new SortedList<DateTime, string>();
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtTask.Text == string.Empty)
+                {
+                    MessageBox.Show("You must endere a task");
+                }
+                else if(taskList.ContainsKey(dtpTaskDate.Value.Date))
+                {
+
+                    MessageBox.Show("Only one task for per date is allowed");
+                    return;
+
+                }
+                else
+                {
+                    taskList.Add(dtpTaskDate.Value.Date, txtTask.Text);
+                    lstTasks.Items.Add(dtpTaskDate.Value.Date);
+                    txtTask.Text = string.Empty;
+                    
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        private void lstTasks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DateTime selectedDate = Convert.ToDateTime(lstTasks.SelectedItem);
+
+            lblTaskDetails.Text = taskList[selectedDate];
+        }
+
+        private void btnRemoveTask_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex == -1)
+            {
+                MessageBox.Show("You must select a task to remove", "Invalid Date");
+            }
+            else
+            {
+                DateTime selectedDate = Convert.ToDateTime(lstTasks.SelectedItem);
+                //string task = txtTask.Text.Trim();
+
+                //taskList.Remove();
+            }
+
+        }
+
+        private void btnPrintAll_Click(object sender, EventArgs e)
+        {
+            string msg = string.Empty;
+            foreach (var s in taskList)
+            {
+                msg += ($"{s.Key} {s.Value}\n");
+            }
+            MessageBox.Show(msg);
+        }
     }
 }
